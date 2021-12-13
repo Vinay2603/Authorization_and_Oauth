577110fc2b9ddd15773b5d2b3213d30b4829373f
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-12-13 13:52:52 +0530

    add

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -7,14 +7,18 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const User = require("../models/user")
 
 passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://yourdomain:3000/auth/google/callback",
+    clientID:     process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://yourdomain:2233/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
+  
+     
+        console.log("accessToken,  refreshToken, profile",accessToken, refreshToken, profile)
+        return done(err, "user");
+   
   }
 ));
+
+module.exports = passport
